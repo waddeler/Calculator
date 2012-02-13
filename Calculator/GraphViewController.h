0ca4c3fd4a5932fe68d8e0a0d9c1940aa37bc9b6
Paul Fellows <paulfellows67@unknown-60-c5-47-08-93-8a.home>
2012-02-13 16:15:34 +0000

    Thi seas purely adding the second MVC and embedding in  a Navigation controller along with removing unnecessary buttons from the calculator UI

---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by Paul Fellows on 13/02/2012.
+//  Copyright (c) 2012 Lichfield Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
